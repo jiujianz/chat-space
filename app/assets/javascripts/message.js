979d12242f5ffabe431feb964a9d0da1616437c0
jiujianz <cocobskg@gmail.com>
2019-03-08 16:31:48 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,5 @@
 $(function() {
-	function buildSendMessageHTML(message){
+	function GroupsMessageHTML(message){
         var api_message = (message.image) ? `<img src="${message.image}" >` :``;
 		var html = `
 		            <div class= "message__right--block--chat__box">
@@ -15,6 +15,30 @@ $(function() {
 				    </div>`
 	    return html
 	}
+  $(function(){
+    setInterval(update, 5000);
+  });
+  function update(){
+    if($('.messages')[0]){
+      var message_id = $('.messages:last').data('id');
+    } else {
+      var message_id = 0
+    }
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: { id: message_id },
+      dataType: 'json',
+      processData: false,
+        contentType: false
+    })
+    .always(function(group_message){
+      $.each(group_message, function(i,group_message){
+        GroupsMessageHTML(group_message);
+      });
+    });
+  }
+
 	$('#new_message').on('submit', function(e){
 		e.preventDefault();
 		  var formData = new FormData(this);
@@ -26,9 +50,9 @@ $(function() {
 	      dataType: 'json',
 	      processData: false,
           contentType: false
-        })
+      })
         .done(function(user_message){
-        	var html = buildSendMessageHTML(user_message);
+        	var html = GroupsMessageHTML(user_message);
         	$('.messages').append(html)
         	$('#message_content').val('')
 	        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
