696fbd5f8c8866ec01c0433fdd8705ad2bc0409b
jiujianz <cocobskg@gmail.com>
2019-03-14 21:37:52 +0900

    a

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-  server '<13.112.135.87', user: 'ec2-user', roles: %w{app db web}
+  server '13.112.135.87', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
