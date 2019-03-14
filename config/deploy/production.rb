f4e5362c7144da7201714ddfa5f34cfe2bad4379
jiujianz <cocobskg@gmail.com>
2019-03-14 17:47:56 +0900

    変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '54.64.140.126', user: 'ec2-user', roles: %w{app db web}
+server '13.112.135.87', user: 'ec2-user', roles: %w{app db web}
 
 
 
